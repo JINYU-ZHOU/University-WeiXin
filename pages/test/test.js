20291bd5291df0e6aeb4ba0a25152bdd3fab0e9f
sunxingyu <1004701224@qq.com>
2020-03-30 08:42:22 +0800

    整合腾讯地图组件
    组件测试可以正常使用，test为测试文件

---
+++ pages/test/test.js
@@ -0,0 +1,15 @@
+// pages/test/test.js
+const key = 'AHJBZ-NMCYX-UO44G-ZC66D-YMS5V-C7FKB'; //使用在腾讯位置服务申请的key
+const referer = '大学生一站式平台'; //调用插件的app的名称
+const location = JSON.stringify({
+  latitude: 39.89631551,
+  longitude: 116.323459711
+});
+const category = '生活服务,娱乐休闲';
+
+wx.navigateTo({
+  url: 'plugin://chooseLocation/index?key=' + key + '&referer=' + referer + '&location=' + location + '&category=' + category
+});
+Page({
+ 
+})
