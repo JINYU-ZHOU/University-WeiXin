00e9e61da9c4ade2aae39089074011d7596621c4
sunxingyu <1004701224@qq.com>
2020-04-03 20:17:05 +0800

    完善用户登录态记录，完善修改用户信息接口

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # University-WeiXin
 大学生一站式平台小程序端
+后台服务器连接地址：https://spergol.com/
+数据库名称：university
--- app.js
+++ app.js
@@ -9,44 +9,64 @@ App({
     // 登录
     wx.login({
       success: req => {
-            // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-                // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        wx.request({
-          url: 'https://spergol.com/login',
+        // 获取用户信息
+        wx.getSetting({
+          success: res => {
+            if (res.authSetting['scope.userInfo']) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+              wx.getUserInfo({
+                success: res => {
+                  // 可以将 res 发送给后台解码出 unionId
+                  this.globalData.userInfo = res.userInfo
+                  wx.setStorageSync('username', res.userInfo.nickName)
+                  // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                  wx.request({
+                    url: 'https://spergol.com/login',
 
-          data: {
-            code: req.code,
-            appid:'wxa1b202ae01b354d3',
-            secret:'c018bc3857e272854e69796662a92d4d',
-            username:res.userInfo.nickName
-          },
-          method: 'GET',
-          success: (res) => {
-            console.log(res.data)
+                    data: {
+                      code: req.code,
+                      appid: 'wxa1b202ae01b354d3',
+                      secret: 'c018bc3857e272854e69796662a92d4d',
+                      username: wx.getStorageSync('username')
+                    },
+                    header:{
+                      'content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    method: 'POST',
+                    success: (res) => {
+                      console.log(res.data)
+                      if(res && res.header && res.header['Set-Cookie']){
+                        wx.setStorageSync('cookieKey', res.header['Set-Cookie']);//保存Cookie到Storage
+                      }
+                      console.log("cookie:"+wx.getStorageSync('cookieKey'))
+
+                    }
+                  })
+                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                  // 所以此处加入 callback 以防止这种情况
+                  if (this.userInfoReadyCallback) {
+                    this.userInfoReadyCallback(res)
+                  }
+                }
+              })
+            }
           }
         })
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-        
-      }
-    })
 
+      }
+    })
+    //修改用户信息
+    wx.request({
+      url: 'https://spergol.com/change',
+      header:{
+        'content-Type': 'application/x-www-form-urlencoded',
+        'Cookie':wx.getStorageSync('cookieKey')
+      },
+      method: 'POST',
+      data:{
+        money:'501'
+      }
+    })
   },
   globalData: {
     userInfo: null
