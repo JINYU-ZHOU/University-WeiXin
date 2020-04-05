349ebc9a35bfabc4a7f9956e332066ddcee7f4c4
sunxingyu <1004701224@qq.com>
2020-04-05 11:03:57 +0800

    完善信息上传
    test页面开启时会上传发布信息，服务器暂未上传对应接口，访问会报错，本地运行时需屏蔽test文件

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,4 +1,5 @@
 // pages/test/test.js
+const chooseLocation = requirePlugin('chooseLocation');//地图信息
 const key = 'AHJBZ-NMCYX-UO44G-ZC66D-YMS5V-C7FKB'; //使用在腾讯位置服务申请的key
 const referer = '大学生一站式平台'; //调用插件的app的名称
 const location = JSON.stringify({
@@ -10,6 +11,26 @@ const category = '生活服务,娱乐休闲';
 wx.navigateTo({
   url: 'plugin://chooseLocation/index?key=' + key + '&referer=' + referer + '&location=' + location + '&category=' + category
 });
+//页面加载的时候提交用户数据，接口已完善
 Page({
+ onShow: function(){
+  const location = JSON.stringify(chooseLocation.getLocation()); 
+  console.log(location)
+  wx.request({
+    url: 'http://localhost:8080/University/main',
+    data:{
+      username:wx.getStorageSync('username'),
+      telephone:'12345678910',
+      lftime:'2020-03-15',
+      map:location,
+      
+    },
+    method:'POST',
+    header:{
+      'content-Type': 'application/x-www-form-urlencoded',
+      'Cookie':wx.getStorageSync('cookieKey')
+    }
  
+  })
+ }
 })
